23ae3864971f011407125f697617b5825a6d2e9a
Richard Crowder <riccro@gmail.com>
2016-10-17 16:02:29 +0100

    Copyright header updates

--- .travis/install_deps.sh
+++ .travis/install_deps.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# OgmaNeo
+# OgmaNeoDemos
 # Copyright(c) 2016 Ogma Intelligent Systems Corp. All rights reserved.
 #
-# This copy of OgmaNeo is licensed to you under the terms described
-# in the OGMANEO_LICENSE.md file included in this distribution.
+# This copy of OgmaNeoDemos is licensed to you under the terms described
+# in the OGMANEODEMOS_LICENSE.md file included in this distribution.
 
 set -ex
 
